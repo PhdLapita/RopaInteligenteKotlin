54e9d99627b11b5cc61aa2fe940d2e4f894d8c7f
PhdLapita <glapav@uni.pe>
2017-07-27 12:06:51 -0500

    ahora si subi todo??

---
+++ app/src/main/java/com/bearcreekmining/ropainteligente/ropainteligente/view/ui/actividades/OxigeActivity.kt
@@ -0,0 +1,13 @@
+package com.bearcreekmining.ropainteligente.ropainteligente.view.ui.actividades
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bearcreekmining.ropainteligente.ropainteligente.R
+
+class OxigeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oxige)
+    }
+}
